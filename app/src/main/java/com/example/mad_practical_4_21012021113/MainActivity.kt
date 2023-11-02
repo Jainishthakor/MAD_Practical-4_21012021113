96f74df3e2629a6a4b1099534a43a95806d4c152
JAINISH <jainishthakor6841@gmail.com>
2023-11-02 22:45:30 +0530

    update

--- app/src/main/java/com/example/mad_practical_4_21012021113/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012021113/MainActivity.kt
@@ -1,22 +1,24 @@
 package com.example.mad_practical_4_21012021113
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login: Button = findViewById<Button>(R.id.login)
+        val signup: Button = findViewById<Button>(R.id.signup)
 
-        var button = findViewById<View>(R.id.button)
-        button.setOnClickListener {
-            intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-
+        login.setOnClickListener {
+            Intent(this@MainActivity, LoginActivity::class.java).also { startActivity(it) }
+        }
+        signup.setOnClickListener {
+            Intent(this@MainActivity, SignupActivity::class.java).also { startActivity(it) }
         }
     }
-
 }
--- app/src/main/java/com/example/mad_practical_4_21012021113/registerActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012021113/registerActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mad_practical_4_21012021113
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class registerActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val login: Button = findViewById<Button>(R.id.loginbutton1)
+//        val register:Button=findViewById<Button>(R.id.registerbutton)
+        login.setOnClickListener {
+            Intent(this@SignupActivity, LoginActivity::class.java).also { startActivity(it) }
+        }
+//        register.setOnClickListener{
+//            Intent(this@signupActivity,)
+//        }
+
     }
 }
